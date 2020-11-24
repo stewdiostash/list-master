925f00c1ca9037e4b65762dfed4cfef718aacbed
Andrew Stewart <1andrewstewart@gmail.com>
2020-11-24 08:23:03 -0500

    html template created, vars declared, add task flow defined

---
+++ app.js
@@ -0,0 +1,45 @@
+// Define UI variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener("submit", addTask);
+}
+
+// Add task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = "<i class='fa fa-remove'></i>";
+  // Append link to li
+  li.appendChild(link);
+
+  console.log(li);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
