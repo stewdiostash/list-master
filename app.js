2c8312d70d182fe107f1f8bb8246c60b382a4256
Andrew Stewart <1andrewstewart@gmail.com>
2020-11-24 10:39:56 -0500

    filter task function added

--- app.js
+++ app.js
@@ -59,10 +59,23 @@ function removeTask(e) {
 }
 
 // Clear tasks
-function clearTasks(e) {
+function clearTasks() {
   //   taskList.innerHTML = "";
 
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
